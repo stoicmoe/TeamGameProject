bb5feb57ec7ecd6bbc0443c6e025e7fa2e6a3cde
stoicmoe <adham.elhelaly@student.guc.edu.eg>
2026-03-02 23:59:11 +0200

    test 4

--- CS GAME/src/testpackage/testclass2.java
+++ CS GAME/src/testpackage/testclass2.java
@@ -2,5 +2,8 @@ package testpackage;
 
 public class testclass2 {
 	public static void main (String[] args){
- System.out.println("Hello World");}
+ System.out.println("Hello World");
+ int x =2;
+ int y = 4;
+ System.out.println(y/x);}
 }
