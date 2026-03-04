2eeba8e9c14e88eb30880881aceb886ac5f1096f
bushr <bushr@Bushra_Laptop>
2026-03-04 13:13:30 +0300

    bb

---
+++ CS GAME/src/game/engine/Constants.java
@@ -0,0 +1,9 @@
+package game.engine;
+
+public final class Constants {
+	int BOARD_SIZE = 100;
+	int BOARD__ROWS = 10;
+	int BOARD_COLS = 10;
+	int WINNING_POSITION = 99;
+	int STARTING_POSITION = 0;
+}
