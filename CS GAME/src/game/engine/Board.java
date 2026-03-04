a42128791f85744c7fa416d8bf7cb0e762778d5a
bushr <bushr@Bushra_Laptop>
2026-03-04 14:20:06 +0300

    Game (still not done)

---
+++ CS GAME/src/game/engine/Board.java
@@ -0,0 +1,36 @@
+package game.engine;
+
+import java.util.ArrayList;
+import game.engine.cells.*;
+import game.engine.monsters.*;
+import game.engine.cards.*;
+
+public class Board {
+	private Cell[][] boardCells;
+	private static ArrayList<Monster> stationedMonsters;
+	private static ArrayList<Card> originalcards;
+	private static ArrayList<Card> cards;
+	
+	public Cell[][] getBoardCells() {
+		return boardCells;
+	}
+	public static ArrayList<Monster> getStationedMonsters() {
+		return stationedMonsters;
+	}
+	public static void setStationedMonsters(ArrayList<Monster> stationedMonsters) {
+		Board.stationedMonsters = stationedMonsters;
+	}
+	public static ArrayList<Card> getOriginalcards() {
+		return originalcards;
+	}
+	public static ArrayList<Card> getCards() {
+		return cards;
+	}
+	public static void setCards(ArrayList<Card> cards) {
+		Board.cards = cards;
+	}
+	
+	public Board(ArrayList<Card> readCards){
+		boardCells = new Cell[Constants.BOARD_ROWS][Constants.BOARD_COLS];
+	}
+}
--- CS GAME/src/game/engine/Constants.java
+++ CS GAME/src/game/engine/Constants.java
@@ -2,24 +2,24 @@ package game.engine;
 
 public final class Constants {
 	//Board Constants: 
-	public int BOARD_SIZE = 100;
-	public int BOARD__ROWS = 10;
-	public int BOARD_COLS = 10;
-	public int WINNING_POSITION = 99;
-	public int STARTING_POSITION = 0;
+	public static final int BOARD_SIZE = 100;
+	public static final int BOARD_ROWS = 10;
+	public static final int BOARD_COLS = 10;
+	public static final int WINNING_POSITION = 99;
+	public static final int STARTING_POSITION = 0;
 	//Special Cells Positions Constants:
-	public int[] MONSTER_CELL_INDICES = {2,18,34,54,82,88}; 
-	public int[] CONVEYOR_CELL_INDICES = {2,18,34,54,82,88}; //(Start of the belt)
-	public int[] SOCK_CELL_INDICES = {32, 42, 74, 84, 98}; //(Start of the sock)
-	public int[] CARD_CELL_INDICES = {4, 12, 28, 36, 48, 56, 60, 76, 86, 90};
+	public static final int[] MONSTER_CELL_INDICES = {2,18,34,54,82,88}; 
+	public static final int[] CONVEYOR_CELL_INDICES = {2,18,34,54,82,88}; //(Start of the belt)
+	public static final int[] SOCK_CELL_INDICES = {32, 42, 74, 84, 98}; //(Start of the sock)
+	public static final int[] CARD_CELL_INDICES = {4, 12, 28, 36, 48, 56, 60, 76, 86, 90};
 	//Energy Constants:
-	public int WININNG_ENERGY = 1000;
-	public int MIN_ENERGY = 0;
+	public static final int WININNG_ENERGY = 1000;
+	public static final int MIN_ENERGY = 0;
 	//Monster Constants:
-	public int MULTITASKER_BONUS = 200;
-	public int SCHEMER_STEAL = 10;
+	public static final int MULTITASKER_BONUS = 200;
+	public static final int SCHEMER_STEAL = 10;
 	//Cell Constants:
-	public int SLIP_PENALTY = 100;
+	public static final int SLIP_PENALTY = 100;
 	//Power Constants: 
-	public int POWERUP_CONST = 500;
+	public static final int POWERUP_CONST = 500;
 }
