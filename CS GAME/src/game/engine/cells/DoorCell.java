327022e6d650a612b95478917d561254ceb59c3b
Miryam <miriam.ahmed@student.guc.edu.eg>
2026-03-08 21:33:34 +0300

    door cell

--- CS GAME/src/game/engine/cells/DoorCell.java
+++ CS GAME/src/game/engine/cells/DoorCell.java
@@ -1,5 +1,26 @@
 package game.engine.cells;
 
-public class DoorCell {
+public class DoorCell extends Cell{
+	Role role;
+	int energy;
+	boolean activated;
 
+	public DoorCell(String name, Role role, int energy, boolean activated) {
+		super(name);
+		this.role = role;
+		this.energy = energy;
+		this.activated = activated;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public int getEnergy() {
+		return energy;
+	}
+	public boolean isActivated() {
+		return activated;
+	}
+	public void setActivated(boolean activated) {
+		this.activated = activated;
+	}
 }
