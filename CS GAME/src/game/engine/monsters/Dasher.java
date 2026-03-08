24877eaaca583393e5c4625beb2c43fc7d65c16a
layan <layan@DESKTOP-F2J4J65>
2026-03-08 21:26:28 +0200

    the rest of .Monsters classes

---
+++ CS GAME/src/game/engine/monsters/Dasher.java
@@ -0,0 +1,30 @@
+package game.engine.monsters;
+
+import game.engine.Role;
+
+public class Dasher extends Monster {
+
+	private int momentumTurns;
+	
+	public Dasher(String name, String description, //Role role,
+			int energy) {
+		super(name, description, //role,
+				energy);
+		this.momentumTurns = 0;
+	}
+
+	public int getMomentumTurns() {
+		return momentumTurns;
+	}
+
+	public void setMomentumTurns(int momentumTurns) {
+		if (momentumTurns < 0)
+			this.momentumTurns = 0;
+		else 
+			this.momentumTurns = momentumTurns;
+	}
+	
+
+	
+	
+}
---
+++ CS GAME/src/game/engine/monsters/MultiTasker.java
@@ -0,0 +1,29 @@
+package game.engine.monsters;
+
+import game.engine.Role;
+
+public class MultiTasker extends Monster {
+	private int normalSpeedTurns;
+
+	public MultiTasker(String name, String description, int energy, //Role role,
+			int normalSpeedTurns) {
+		super(name, description,//role,
+				energy);
+		this.normalSpeedTurns = 0;
+	}
+	
+	public int getNormalSpeedTurns(){
+		return normalSpeedTurns;
+	}
+
+	public void setNormalSpeedTurns(int normalSpeedTurns) {
+		if (this.normalSpeedTurns < 0)
+			this.normalSpeedTurns = 0;
+		else
+			this.normalSpeedTurns = normalSpeedTurns;
+	}
+	
+	
+	
+
+}
