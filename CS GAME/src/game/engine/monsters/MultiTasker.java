a297cd1fdd735f9fb628edf98b9c036898cb02c6
layan <layan@DESKTOP-F2J4J65>
2026-03-09 23:19:47 +0200

    1

--- CS GAME/src/game/engine/monsters/MultiTasker.java
+++ CS GAME/src/game/engine/monsters/MultiTasker.java
@@ -5,7 +5,7 @@ import game.engine.Role;
 public class MultiTasker extends Monster {
 	private int normalSpeedTurns;
 
-	public MultiTasker(String name, String description, Role role, int energy, int normalSpeedTurns) {
+	public MultiTasker(String name, String description, Role role, int energy) {
 		super(name, description,role,energy);
 		this.normalSpeedTurns = 0;
 	}
