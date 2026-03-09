2b9db8efc95c450c57c86d28a61983c62fecceaf
stoicmoe <adham.elhelaly@student.guc.edu.eg>
2026-03-09 23:33:38 +0200

    edit3

--- CS GAME/src/game/engine/dataloader/DataLoader.java
+++ CS GAME/src/game/engine/dataloader/DataLoader.java
@@ -8,9 +8,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 public class DataLoader {
-	public static final String CARDS_FILE_NAME  = "cards.csv";
-    public static final String CELLS_FILE_NAME   = "cells.csv";
-    public static final String MONSTERS_FILE_NAME = "monsters.csv";
+	private static final String CARDS_FILE_NAME  = "cards.csv";
+    private static final String CELLS_FILE_NAME   = "cells.csv";
+    private static final String MONSTERS_FILE_NAME = "monsters.csv";
     
     
     public static ArrayList<Card> readCards() throws IOException{
